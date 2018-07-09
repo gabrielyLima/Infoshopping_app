79d664c00952ed832dfd8207725394771955aa95
aryell dias de menezes <addm@ecomp.poli.br>
2018-07-09 05:20:43 +0000

    TelaTemporaria.js edited online with Bitbucket

--- src/components/TelaTemporaria.js
+++ src/components/TelaTemporaria.js
@@ -18,6 +18,7 @@ import {
   } from 'react-native-material-kit';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Actions } from 'react-native-router-flux';
+import { listar } from '@api/temp';
 
 import { COLORS } from '@constants';
 
@@ -26,20 +27,51 @@ const theme = getTheme();
 const fabSize = Math.min(width, height)/8
 
 export default class TelaTemporaria extends Component{
+    state = {
+        data: [],
+        isLoading: false,
+        width,
+        height
+    };
+
+    fetch(){
+        this.setState({ isLoading: true });
+        listar(this.props.id)
+            .then(response => {
+                response.json()
+                    .then(data => {
+                        if(data){
+                            this.setState({
+                                data: data,
+                                isLoading: false,
+                                shouldFetch: false
+                            });
+                        }
+                    })
+            })
+    }
+
+    componentWillReceiveProps = (nextProps) => {
+        if(nextProps.shouldFetch){
+            this.fetch();
+        }
+    }
+
+    componentDidMount(){
+        this.fetch();
+    }
 
     render(){
-        const teste = this.props.id
+        const id = this.props.id
         return(
             <ScrollView contentContainerStyle={styles.container}>
                 {
                     <FlatList
-                        data={[
-                            {"id":teste}
-                            ]}
+                        data={this.state.data}
                         renderItem={
                             ({item}) =>
                                 <View style={[theme.cardStyle, styles.card]}>
-                                        <Text>{item.id}</Text>
+                                        <Text>{JSON.stringify(item)}</Text>
                                 </View>
                         }
                     />
