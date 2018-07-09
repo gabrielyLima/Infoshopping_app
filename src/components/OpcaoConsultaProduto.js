94f78b39a8bf19ddc52a87808d908eb8667645b8
aryell dias de menezes <addm@ecomp.poli.br>
2018-07-09 01:34:12 +0000

    OpcaoConsultaProduto.js created online with Bitbucket

---
+++ src/components/OpcaoConsultaProduto.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    ScrollView,
+    Dimensions,
+    Alert
+} from 'react-native';
+
+import {
+    getTheme,
+    MKSpinner,
+    MKButton,
+    MKColor
+  } from 'react-native-material-kit';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Actions } from 'react-native-router-flux';
+import { listarProdutos } from '@api/produtos';
+import ItemProduto from '@components/ItemProduto';
+
+import { COLORS } from '@constants';
+
+const {width,  height} = Dimensions.get('window');
+const theme = getTheme();
+const fabSize = Math.min(width, height)/8
+
+export default class OpcaoConsultaProduto extends Component{
+
+    render(){
+        return(
+            <Text>{'aqui deverá ter uma lista de consultas'}</Text>
+        )
+    }
+} 
